9a9cb34067666f250309b333189f9bb5d05cfa19
pcosta19 <32554433+pcosta19@users.noreply.github.com>
2019-02-15 10:31:48 -0500

    Update Country.java

--- Country.java
+++ Country.java
@@ -1,30 +1,55 @@
 public class Country{
-	private String Name;
 	private double GDP;
 	private int NumberOfNukes;
 	private double CINCScore;
 	private double HDIScore;
 	private int NumberOfTopNaturalResources;
 	private String Owner;
+	private String CountryName;
 	
 	//gdp in billions of usd
 	public Country(String name, double gdp, int nukes, double cinc, double hdi, int natrec, String owner)
 	{
-		Name = name;
 		GDP = gdp;
 		NumberOfNukes = nukes;
 		CINCScore = cinc;
 		HDIScore = hdi;
 		NumberOfTopNaturalResources = natrec;
 		Owner = owner;
+		CountryName = name;
 	}
 	
+	public String getCountry()
+	{
+		return CountryName;
+	}
 	public double getGDP()
 	{
 		return GDP;
 	}
-
-	public String getOwner(){
+	
+	public int getNatRec()
+	{
+		return NumberOfTopNaturalResources;
+	}
+	
+	public int getNukes()
+	{
+		return NumberOfNukes;
+	}
+	
+	public double getCINC()
+	{
+		return CINCScore;
+	}
+	
+	public double getHDI()
+	{
+		return HDIScore;
+	}
+	
+	public String getOwner()
+	{
 		return Owner;
 	}
 	

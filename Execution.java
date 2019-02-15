8e4111ec838fd5552bb8f5779554d4f209f6b74f
jack353 <40332192+jack353@users.noreply.github.com>
2019-02-15 10:34:34 -0500

    Update Execution.java

--- Execution.java
+++ Execution.java
@@ -1,12 +1,17 @@
 import java.util.*;
 public class Execution {
+
+	public static ArrayList<Country> LiamCountries;
+	public static ArrayList<Country> RyanCountries;
+	public static ArrayList<Country> SamCountries;
+	public static ArrayList<Country> AustinCountries;
 	public static void main(String[] args)
 	{
 		//define/initialize lists of countries for each person
-		ArrayList<Country> LiamCountries = new ArrayList<Country>();
-		ArrayList<Country> RyanCountries = new ArrayList<Country>();
-		ArrayList<Country> SamCountries = new ArrayList<Country>();
-		ArrayList<Country> AustinCountries = new ArrayList<Country>();
+		LiamCountries = new ArrayList<Country>();
+		RyanCountries = new ArrayList<Country>();
+		SamCountries = new ArrayList<Country>();
+		AustinCountries = new ArrayList<Country>();
 		
 		
 		WorldPower Liam = new WorldPower(LiamCountries);
@@ -14,5 +19,22 @@ public class Execution {
 		WorldPower Sam = new WorldPower(SamCountries);
 		WorldPower Austin = new WorldPower(AustinCountries);
 		
+	}
+
+	private void setOwners(){
+		for (int i = 0; i < CountryList.countryList.size(); i++){
+			if (CountryList.countryList.get(i).getOwner().equals("Austin")){
+				AustinCountries.add(CountryList.countryList.get(i));
+			}
+			else if (CountryList.countryList.get(i).getOwner().equals("Ryan")){
+				RyanCountries.add(CountryList.countryList.get(i));
+			}
+			else if (CountryList.countryList.get(i).getOwner().equals("Liam")){
+				LiamCountries.add(CountryList.countryList.get(i));
+			}
+			else if (CountryList.countryList.get(i).getOwner().equals("Sam")){
+				SamCountries.add(CountryList.countryList.get(i));
+			}
+		}
 	}
 }

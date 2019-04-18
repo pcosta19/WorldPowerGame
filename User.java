158cb2d400554580587bd1527f3449f054d04e84
pcosta19 <32554433+pcosta19@users.noreply.github.com>
2019-04-18 12:29:52 -0400

    updated fundamentals

---
+++ User.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class User {
+	private String name;
+	private ArrayList<Country> countries;
+	
+	public User(String n)
+	{
+		name = n;
+		countries = new ArrayList<Country>();
+	}
+	
+	
+	//be sure to prompt user "Would you like to add a country or a state?"
+	//the response to this determines which method to pass the answer to
+	public void addCountry(String c)
+	{
+		
+	}
+	
+	public void addState(String s)
+	{
+		
+	}
+	

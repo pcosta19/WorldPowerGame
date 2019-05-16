b066d8eec5fae1e72dcfdba1fa34f3cb97dc7625
pcosta19 <32554433+pcosta19@users.noreply.github.com>
2019-05-16 19:40:34 -0400

    Adds getArea()

--- State.java
+++ State.java
@@ -2,10 +2,15 @@
 public class State extends Country{
 	private double LandArea;
 	
-	public State(String n, double gdp, int nukes, double cinc, double hdi, double area, String owner) {
+	public State(String n, double gdp, int nukes, double cinc, double hdi, double area) {
 		super(n, gdp, nukes, cinc, hdi);
 		LandArea = area;
 	}
 	
+	public double getArea()
+	{
+		return LandArea;
+	}
 	
+
 }

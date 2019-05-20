4e873907c0c32c88bbb5e8cab0b62582af3a5b35
pcosta19 <32554433+pcosta19@users.noreply.github.com>
2019-05-20 00:29:24 -0400

    perfect!(?)

--- User.java
+++ User.java
@@ -3,45 +3,108 @@ import java.util.*;
 
 public class User {
 	private String name;
-	private ArrayList<Country> countries;
+	ArrayList<Country> countries;
 	
-	public User(String n)
+	public User()
 	{
-		name = n;
 		countries = new ArrayList<Country>();
 	}
 	
+	public void setName(String n)
+	{
+		name = n;
+	}
+	
+	public static User getUser(String n, User[] u)
+	{
+		int i = 0;
+		for (User r: u)
+		{
+			if (r.getName().equals(n))
+				return u[i];
+			i++;
+		}
+		
+		return null;
+	}
+
+	private String getName() {
+		
+		return name;
+	}
+
 	Scanner keyboard = new Scanner(System.in);
-	public void UserTurn()
+	
+	//this is how a user's turn will go during phase 1 of the game
+	public void UserTurnPhaseOne()
 	{
 		System.out.println(name + ", enter the country/state you wish to claim: ");
-		String claim = keyboard.nextLine();
-		Country found = searchCountry(claim, 0, CountryList.allCountries.size());
+		
+		//we immediately add the country to the user's list of countries
+		//we can then access this addition by assigning it to the variable newAddition
+		//which allows us to access it and its values in subsequent lines
+		addCountry(keyboard.nextLine());
+		Country newAddition = countries.get(countries.size()-1);
 		
 		//check if what they have claimed is a State, if it is, subtract the area of that state
 		//from the total area able to be claimed per turn
-		//MUST IMPLEMENT LINE(S) TO GIVE OPTION TO USER TO END THEIR TURN
-		if (found instanceof State)
+		if (newAddition instanceof State)
 		{
-			double availableArea = 350000;
+			//area of alaska if about 650000, so we need maximum to be a little more than that
+			//availableArea takes into account what has just been claimed
+			double availableArea = 700000 - ((State) newAddition).getArea();
+			
+			/*The user now continues to claim states until the maximum land area is reached.
+			 * If the user would like to end their turn early (i.e. if they only have 5 miles remaining),
+			 * they may type 'end' to end their turn. Furthermore, if they try to claim a country,
+			 * they will enter a while loop and receive a message saying they must claim a state.
+			 */
 			while (availableArea > 0)
 			{
-				if (((State) found).getArea() < availableArea) 
-				{
-					availableArea -= ((State) found).getArea();
-					countries.add(found);
-				}
-				else
-					System.out.println("Sorry, this state's land area goes over your maximum turn claim size.");
+				
 				System.out.println("You have " + availableArea + "square miles left to claim, "
-						+ "which state would you like to claim?");
-				claim = keyboard.nextLine();
-				found = searchCountry(claim, 0, CountryList.allCountries.size());
+						+ "which state would you like to claim? If you would like to finish claiming this turn, type 'end'");
+				if (keyboard.nextLine().equals("end"))
+					break;
+				
+				//newAddition is given a new value
+				else{
+					addCountry(keyboard.nextLine());
+					newAddition = countries.get(countries.size()-1);
+				}
+				
+				//makes the user type the name of a state if they typed in the name of a country
+				//newAddition is assigned a new value accordingly
+				while (newAddition instanceof Country)
+				{
+					countries.remove(newAddition);
+					System.out.println("Sorry, you must enter a state. Try again: ");
+					addCountry(keyboard.nextLine());
+					newAddition = countries.get(countries.size()-1);
+				}
+				
+				//if the state they have attempted to add a state with too much area,
+				//we remove the state from their list and the loop begins again, reminding them of their available area
+				if (((State) newAddition).getArea() > availableArea) {
+					System.out.println("Sorry, this state's land area goes over your maximum turn claim size.");
+					countries.remove(newAddition);
+				}
+				
+				else
+					availableArea -= ((State) newAddition).getArea();
 			}
-			
 		}
 		
-		
+		//gives the user the option to trade countries with other players
+		//it is assumed that the players have reached a verbal agreement on the trade
+		//the other user will not have to confirm the trade, we are basing this off of honesty
+		System.out.println("If you would like to trade countries with another user, type 'trade', "
+				+ "otherwise, please type 'end' to complete your turn: ");
+		while (!keyboard.nextLine().equals("end"))
+		{
+			System.out.println("If you would like to trade countries with another user, type 'trade', "
+					+ "otherwise, please type 'end' to complete your turn: ");
+		}
 	}
 	
 	//binary search
@@ -78,6 +141,25 @@ public class User {
 		
 	}
 	
+	public void removeCountry(String c) 
+	{
+		countries.remove(searchCountry(c, 0, CountryList.allCountries.size()));
+	}
+	
+	public double getPower() 
+	{		
+		double power = 0;	
+		for (Country c: countries) 
+		{
+				
+		//this equation determines the percent of the world power this user controls		
+		power += (c.getCINC() * 100) + ((c.getGDP() / 5000000)/1000000) + 
+				(c.getHDI() * 3) + Math.log(c.getNukes() * 10);
+		}	
+		return power;
+	}
+		
+
 	//list all the countries a user has
 	public String listCountries()
 	{
@@ -94,4 +176,6 @@ public class User {
 	{
 		return name + ": " + listCountries();
 	}
+
+	
 }
